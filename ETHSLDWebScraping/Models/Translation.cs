abf127776e8e64b999dfbeae013216d68e3e223a
nabet-yeking <nahom.yengesu.learn@gmail.com>
2025-08-19 15:17:52 +0300

    defining translation model

--- ETHSLDWebScraping/Models/Translation.cs
+++ ETHSLDWebScraping/Models/Translation.cs
@@ -8,5 +8,10 @@ namespace ETHSLDWebScraping.Models
 {
     internal class Translation
     {
+        public int Id { get; set; }
+        public string? TransTerm { get; set; }
+        public string? TransMeaning { get; set; }
+        public string? Code { get; set; }
+        public string? Name { get; set; }
     }
 }

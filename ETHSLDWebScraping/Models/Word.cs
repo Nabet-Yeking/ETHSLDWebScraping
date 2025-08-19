961f2e54a4a0f9c338f6bb6213f6fee3576d31b6
nabet-yeking <nahom.yengesu.learn@gmail.com>
2025-08-19 15:13:03 +0300

    Defining word model

--- ETHSLDWebScraping/Models/Word.cs
+++ ETHSLDWebScraping/Models/Word.cs
@@ -8,5 +8,14 @@ namespace ETHSLDWebScraping.Models
 {
     internal class Word
     {
+        public int Id { get; set; }
+        public string Term { get; set; }
+        public string Grammatical_Class { get; set; }
+        public string? Meaning { get; set; }
+        public int Status { get; set; }
+        public int typeID { get; set; }
+        public DateTime Updated_At { get; set; }
+        public DateTime Created_At { get; set; }
+        public string Type { get; set; }
     }
 }

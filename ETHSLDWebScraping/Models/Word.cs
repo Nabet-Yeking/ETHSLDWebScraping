78bb355bd7b009da48c6001483cadfe189893ffe
nabet-yeking <nahom.yengesu.learn@gmail.com>
2025-08-19 19:52:46 +0300

    Changing members data type

--- ETHSLDWebScraping/Models/Word.cs
+++ ETHSLDWebScraping/Models/Word.cs
@@ -9,13 +9,13 @@ namespace ETHSLDWebScraping.Models
     internal class Word
     {
         public int Id { get; set; }
-        public string Term { get; set; }
-        public string Grammatical_Class { get; set; }
+        public string Term { get; set; } = string.Empty;
+        public string Grammatical_Class { get; set; } = string.Empty;
         public string? Meaning { get; set; }
         public int Status { get; set; }
         public int typeID { get; set; }
-        public DateTime Updated_At { get; set; }
-        public DateTime Created_At { get; set; }
-        public string Type { get; set; }
+        public string Updated_At { get; set; } = string.Empty;
+        public string Created_At { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
     }
 }

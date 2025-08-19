7fc092bda54298832f3ed39ca5b4dd979dee9f13
nabet-yeking <nahom.yengesu.learn@gmail.com>
2025-08-19 15:40:17 +0300

    Defining WordDetailResponseDto

--- ETHSLDWebScraping/DTOs/WordDetailResponseDto.cs
+++ ETHSLDWebScraping/DTOs/WordDetailResponseDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ETHSLDWebScraping.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace ETHSLDWebScraping.DTOs
 {
-    internal class WordDetailResponseDto
+    internal sealed class WordDetailResponseDto
     {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public WordDetailData? Data { get; set; }
+
+        public sealed class WordDetailData
+        {
+            public int Id { get; set; }
+            public string? Term { get; set; }
+            public string? Meaning { get; set; }
+            public string? Type { get; set; }
+            public int Status { get; set; }
+            public Content[]? Contents { get; set; }
+            public Translation[]? Translations { get; set; }
+        }
     }
 }
